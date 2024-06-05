80fb95c72ecf278cf4c68ecc03c2de218d4f2c4d
Samantha <Samantha@Luigi>
2024-06-05 17:06:01 -0600

    Se implementaron las clases SimplePieceFactory y Bishop. Se creó clase Queen

--- src/main/java/itcr/chess/SimplePieceFactory.java
+++ src/main/java/itcr/chess/SimplePieceFactory.java
@@ -7,29 +7,16 @@ package itcr.chess;
 /**
  *
  * @author Samantha
- 
+ */
 public class SimplePieceFactory {
-    public Piece createPiece(PieceTypes piece, ChessColor color){
-            switch(piece){
-                case PAWN -> {
-                    return new Pawn(color);
-            }
-                case ROOK -> {
-                    return new Rook(color);
-            }
-                case KNIGHT -> {
-                    return new Knight(color);
-            }
-                case BISHOP -> {
-                    return new Bishop(color);
-            }
-                case QUEEN -> {
-                    return new Queen(color);
-            }
-                case KING -> {
-                    return new King(color);
-            }
-            }
-        }
+    public Piece createPiece(PieceTypes piece){
+     return piece == PieceTypes.PAWN ? new Pawn():  
+     return piece == PieceTypes.ROOK ? new Rook():       
+     return piece == PieceTypes.KNIGHT ? new Knight():  
+     return piece == PieceTypes.BISHOP ? new Bishop():  
+     return piece == PieceTypes.QUEEN ? new Queen():  
+     return piece == PieceTypes.KING ? new King():  
+        null;
+     
+    }
 }
-*/
